56d975473ea24d4587ed282be3938a5df5bbdcee
Mo2Adam <mohamedadam867@yahoo.com>
2022-04-02 15:00:08 +0200

    added the footer seciton and review section

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// let slides = document.querySelectorAll('.slide-review');
+
+
+// console.log(slides);
+
+// function slideShow(){
+
+// slides.forEach(slide => {
+//     slide.style.left = "200%";
+    
+// });
+
+// }
+
+
+
+// setTimeout(slideShow, 300);
